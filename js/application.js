df5683961fe281f3ce9752a4ec659d35999927a3
Edoe Cohen <edoecohen@gmail.com>
2014-09-10 23:07:26 -0700

    Added more jQuery functions
    - Ability to add items
    - Reset input field after adding
    - Red alert when entering without an item
    - Show special features when hovering

--- js/application.js
+++ js/application.js
@@ -1,12 +1,36 @@
 $(document).ready(function() {
 
+
 // ADD LIST ITEM
+	
+   var addItem = function() {
+		$("#add").on("click", function() {
+			var item = $('#entry').val();
+			
+			if (item!="") {
+				var item_listed = "<li class='active'><div class='handle'></div><div class='box'><img src='images/check.png' /></div>" + item + "<div class='delete'><img src='images/delete.png' /></div></li>"
+				$("ul").prepend(item_listed);
+				$('#entry').val('').focusout();
+			}
 
-	$("#add").on("click", function() {
-		var item = $('#entry').val();
-		var item_listed = "<li class='active'><div class='handle'></div><div class='box'><img src='images/check.png' /></div>" + item + "<div class='delete'><img src='images/delete.png' /></div></li>"
-		$("ul").prepend(item_listed);
-		$('#entry').val('').focusout();
-	}); 
+			else {
+				$(".list").animate({"height":"342px", "margin-top":"10px"}, "fast")
+				.delay(2950).animate({"height":"390px","margin-top":"15px"});
+				$(".alert").slideDown("fast").delay(3000).slideUp("fast");
+			};
+		}); 
+   };
 
+   addItem();
+
+
+// ADD LIST ITEM WITH ENTER KEY
+
+	$("#entry").keydown(function (e) {
+		if (e.keyCode == 13) { 
+			$("#add").click();
+		}
+	})
+	
 });
+
